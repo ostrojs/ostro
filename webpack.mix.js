417a4861a5d2229efe1781d50981fc9af7deaf82
ostrojs <ostrojsframework@gmail.com>
2021-12-18 02:58:50 +0530

    comment update for webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,7 +6,7 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your OstroJS applications. By default, we are compiling the CSS
  | file for the application as well as bundling up all the JS files.
  |
  */
