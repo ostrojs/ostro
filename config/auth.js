0d8e913316da61107de75fe092a209c0c72d9854
Amar kumar singh <amar@ostrojs.com>
2025-07-24 13:53:44 +0530

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -12,7 +12,7 @@ module.exports = {
     */
 
     'defaults': {
-        'guard': 'web',
+        'guard': 'admin',
     },
 
     /*
@@ -34,10 +34,11 @@ module.exports = {
 
     'guards': {
 
-        'web': {
+        'admin': {
             'driver': 'session',
-            'provider': 'users',
+            'provider': 'admins',
         },
+        
 
         'api': {
             'driver': 'token',
@@ -65,11 +66,48 @@ module.exports = {
 
     'providers': {
 
-        'users': {
+        'admins': {
             'driver': 'eloquent',
-            'model': require('~/app/models/user'),
+            'model': require('~/app/models/admin'),
         },
 
-    }
+    },
+
+    /*
+    |--------------------------------------------------------------------------
+    | Resetting Passwords
+    |--------------------------------------------------------------------------
+    |
+    | You may specify multiple password reset configurations if you have more
+    | than one user table or model in the application and you want to have
+    | separate password reset settings based on the specific user types.
+    |
+    | The expire time is the number of minutes that the reset token should be
+    | considered valid. This security feature keeps tokens short-lived so
+    | they have less time to be guessed. You may change this as needed.
+    |
+    */
+
+    'passwords': {
+        'admin': {
+            'provider': 'admin',
+            'table': 'password_resets',
+            'expire': 60,
+            'throttle': 60,
+        },
+    },
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Confirmation Timeout
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define the amount of seconds before a password confirmation
+    | times out and the user is prompted to re-enter their password via the
+    | confirmation screen. By default, the timeout lasts for three hours.
+    |
+    */
+
+    'password_timeout': 10800,
 
 };
