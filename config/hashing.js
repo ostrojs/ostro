02204bffbb7de5bad993fc3409cc309537ab2d5b
Amar kumar singh <amar@ostrojs.com>
2025-07-24 20:02:45 +0530

    Update hashing.js

--- config/hashing.js
+++ config/hashing.js
@@ -5,57 +5,39 @@ module.exports = {
     | Default Hash Driver
     |--------------------------------------------------------------------------
     |
-    | This option specifies which hashing algorithm will be used by default
-    | when hashing passwords or other sensitive data in your application.
-    | 
-    | Supported options:
-    | - "bcrypt": Uses the bcrypt algorithm, which is a strong adaptive
-    |   hashing function designed for password hashing.
-    | - "crypto": Uses the Node.js built-in crypto module for hashing, 
-    |   which may use different algorithms (e.g., SHA256). Generally faster,
-    |   but less secure for password hashing compared to bcrypt.
-    | 
-    | You can change this value depending on your security and performance needs.
+    | Specifies the default hashing algorithm to use for passwords or sensitive data.
+    | Supported drivers:
+    | - "bcrypt": Strong adaptive password hashing.
+    | - "crypto": Uses Node.js crypto module (e.g., SHA256). Faster but less secure for passwords.
     |
     */
-
-    'driver' : 'crypto',
+    'driver': 'crypto',
 
     /*
     |--------------------------------------------------------------------------
     | Bcrypt Options
     |--------------------------------------------------------------------------
     |
-    | Configuration options for the bcrypt hashing algorithm. The "rounds"
-    | option controls the cost factor which influences the time required
-    | to compute the hash. Higher rounds increase security but also CPU load.
-    |
-    | Typical values are between 8 and 12. Adjust this according to your
-    | application’s performance and security requirements.
+    | Options for the bcrypt algorithm. The "rounds" value controls the cost factor,
+    | increasing security but also CPU usage. Recommended range is 8-12.
     |
     */
-
-    'bcrypt' : {
-        'rounds' : env('BCRYPT_ROUNDS', 8),
+    'bcrypt': {
+        'rounds': env('BCRYPT_ROUNDS', 8),
     },
-    
+
     /*
     |--------------------------------------------------------------------------
     | Crypto Options
     |--------------------------------------------------------------------------
     |
-    | Configuration options for the crypto hashing driver. The "rounds"
-    | parameter here could refer to the number of iterations or key stretching
-    | rounds depending on the algorithm used internally.
-    |
-    | Since "crypto" is a generic hashing mechanism, the exact effect of this
-    | setting may vary. It is usually faster than bcrypt but may not provide
-    | the same level of security for password storage.
+    | Options for the crypto driver. The "rounds" value can control iterations or
+    | key stretching depending on implementation. Usually faster but less secure
+    | than bcrypt for password hashing.
     |
     */
-
-    'crypto' : {
-        'rounds' : env('BCRYPT_ROUNDS', 10),
+    'crypto': {
+        'rounds': env('CRYPTO_ROUNDS', 10),
     }
 
 }
